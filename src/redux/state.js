1b029855e50d0929dd9da94aeaf06cc09c9be5a0
Ruben Arcos <ruben.arcos06@gmail.com>
2023-09-21 12:10:27 -0500

    created more files on components, containers, and redux so I can start setting up project. added state data on redux folder.

---
+++ src/redux/state.js
@@ -0,0 +1,38 @@
+const initialState = {
+  businesses: [
+    {
+      id: 1,
+      name: "El Si Hay",
+      description:
+        "Popular, no-frills Mexican joint doling out a variety of street tacos from a walk-up window.",
+      address: "601 W Davis St, Dallas, TX 75208",
+      hours: "10AM - 11PM",
+    },
+    {
+      id: 2,
+      name: "Tacos La Banqueta",
+      description:
+        "Ask any Dallas foodie who has been around long enough, and they will name Tacos La Banqueta as one of the best taco institutions in this city. It might not be the first place that catches your eye, but the tacos are phenomenal. Get the Cabeza, aka cow head meat, lengua, aka beef tongue, and suadero, aka a braised cut of meat with charred edges.",
+      address: "1305 N Carroll Ave, Dallas, TX 75204",
+      hours: "9AM - 11PM",
+    },
+    {
+      id: 3,
+      name: "Revolver Taco Lounge",
+      description:
+        "Another native-Fort Worthian institution, Revolver Taco Lounge finally made its way to Deep Ellum across from Pecan Lodge. It has expanded its menu from what used to be just 6 tacos to a super extensive list of tacos, sides, and unique beverages. Our choice taco to get while there? The Al Pastor!",
+      address: "2701 Main St #120, Dallas, TX 75226",
+      hours: "11AM - 10PM",
+    },
+    {
+      id: 4,
+      name: "Taqueria La Ventana",
+      description: `One of Dallas’s gems set behind the historic El Fenix restaurant, La Ventana has a Miami-type vibe with hanging lights, colorful decor, and lively music. And a reason to come beyond the atmosphere and food? The $3.50 margaritas during happy hour! There are several more locations now too, scattered around DFW - we love the one on Cedar Springs for its vibrant patio and murals!`,
+      address: "6507 Circle S Rd, Austin, TX 78745",
+      hours: "11AM - 2AM",
+    },
+  ],
+  username: "",
+};
+
+export default initialState;
