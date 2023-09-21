0fe589a5612f5ed1a5c67e917dc6f488919a6309
Ruben Arcos <ruben.arcos06@gmail.com>
2023-09-21 16:49:46 -0500

    added routers, fixed app.js, added more component and containers.

---
+++ src/Router.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import cookie from "cookie";
+import ListingContainer from "./containers/ListingContainer";
+import DetailsContainer from "./containers/DetailsContainer";
+import LoginContainer from "./containers/LoginContainer";
+import NavigationContainer from "./containers/NavigationContainer";
+import AddContainer from "./containers/NavigationContainer";
+import {Link} from 'react-router-dom'
+
+// Write checkAuth function here
+// Check the cookies for a cookie called "loggedIn"
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  return cookies["loggedIn"] ? true : false;
+};
+
+// Write ProtectedRoute function here
+const ProtectedRoute = (props) => {
+  const { component: Component, ...rest } = props;
+  return checkAuth() === true ? <Component {...rest} /> : <Link to="/login" />;
+};
+
+const Router = () => {
+    return (
+        <Routes>
+          <Route path="/" element={ListingContainer} />
+          <Route path="/details/:id" element={<DetailsContainer />} />
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/add" element={<ProtectedRoute component={AddContainer} />} />
+        </Routes>
+      );
+}
+
+export default Router;
