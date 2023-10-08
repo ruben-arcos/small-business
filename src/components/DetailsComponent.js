cd8bdac1b81c240d718c726e1c82746aac168a8d
Ruben Arcos <ruben.arcos06@gmail.com>
2023-10-08 17:51:42 -0500

    done styling details page

--- src/components/DetailsComponent.js
+++ src/components/DetailsComponent.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 export default function DetailsContainer(props) {
-    const location = useLocation();
+  const location = useLocation();
 
-    const taqueria = (props.taquerias.find(t => t.id === Number(location.state.id)))
+  const taqueria = props.taquerias.find(
+    (t) => t.id === Number(location.state.id)
+  );
 
   console.log(taqueria);
-  return <div>
+  return (
+    <div className="detailsContainer">
+      <div className="detailsInfoContainer">
+        <h2>{taqueria.name}</h2>
+        <h3>{taqueria.address}</h3>
+        <h3>{taqueria.hours}</h3>
+        <p className="descriptionText">{taqueria.description}</p>
+      </div>
 
-<h2>{taqueria.name}</h2>
-<h3>{taqueria.address}</h3>
-<h3>{taqueria.hours}</h3>
-<p>{taqueria.description}</p>
-<iframe title="map"
-  width="600"
-  height="450"
-  style={{border:0}}
-  loading="lazy"
-  allowFullScreen
-  referrerPolicy="no-referrer-when-downgrade"
-  src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}
-    &q=${taqueria.name}+${taqueria.address}`}>
-</iframe>
-  </div>;
+      <div className="mapContainer">
+        <iframe
+          title="map"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+          src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}
+    &q=${taqueria.name}+${taqueria.address}`}
+        ></iframe>
+      </div>
+    </div>
+  );
 }
